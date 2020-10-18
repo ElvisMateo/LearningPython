3a04fb4e5341e8fe5f27b62b817635dbc19e5a76
Kalvin Mateo <kalvinmateo16@gmail.com>
2020-10-17 23:07:16 -0700

    made documentation adn added to the basics.py

---
+++ Basics/Basics.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+
+#Printing
+
+#print("Hello World")
+#print(30.8)
+#print(20)
+
+#variables
+
+#number = 20
+
+#decimalNumber = 30.8
+
+#word = "hello world"
+
+#letters = 'c'
+
+
+#print(number)
+#print(decimalNumber)
+#print(word)
+#print(letters)
+
+#Integers
+
+#numX = 20
+
+#numY = 30
+
+#numZ = numX + numY
+#numA = numX * numY
+#numB = numY / numX
+
+    
+
+#print(numX)
+#print(numY)
+#print(numZ)
+#print(numA)
+#print(numB)
+
+#Strings
+    #They can be words to sentences to letters
+
+#word1 = "Hello"
+#word2 = "world"
+#space = " "
+
+#concatanation -- adds numbers and makes them into strings
+
+#num1 = "20"
+#num2 = "30"
+
+#newWord = num2 + num1
+
+#num3="09-"
+#num4="26-"
+#num5="03"
+
+
+#irthday = num3+num4+num5
+
+
+#print(newWord)
+#print(birthday)
+
+#Characters or char
+
+#char1 = 'a'
+#char2 = 'b'
+#char3 = 'c'
+
+#num1 = 50
+
+#ogether = ord(char1)
+
+
+#print(together)
+
+#Floats or doubles(java)
+#float1 = 12.0
+#float2 = 23.34
+#float3 = 98.3
+#double = 12.89
+#double1 = 23
+#double2 = 398.2
+
+#print(float1 + double)
+#print(float2 * double1)
+
+
+
+#if and elif and else statements
+    #AND or not
+
+#num = 15
+
+#if(num > 10):
+#    print(num)
+#    num = num - 1
+
+#elif(num == 10):
+#   print("This is 10")
+#if(num > 5):
+#    print("This is bigger than 5")
+#else:
+#    print(num)
+
+#print(num)
+
+#Lists and arrays
+
+fruit = ["apple", "bannana", "pear", "pineapple", "strawberry"]
+fruits = np.array(["apple", "bannana", "pear", "pineapple", "strawberry"])
+
+
+print(fruit)
+print(fruits)
+
+
+
+#inputs and outputs
+
+#Functions   
---
+++ Basics/Installation.md
@@ -0,0 +1,9 @@
+How to install python
+
+
+Step 1:
+    *Install visual studio if you haven't aready at the link provided: https://code.visualstudio.com/download
+    *You can install the extension by typing in python it should be the first item on the. Image below to help:
+
+Step 2: 
+    *Install python on your system by visiting
