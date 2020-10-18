1060a7fb1ff7efb9771f91959ed7c7fa2221461d
Kalvin Mateo <kalvinmateo16@gmail.com>
2020-10-17 20:33:55 -0700

    Basics file is in teh folder

---
+++ Basics.py
@@ -0,0 +1,56 @@
+
+#Printing
+
+#print("Hello World")
+#print(30.8)
+#print(20)
+
+#variables
+
+#number = 20
+
+#decimalNumber = 30.8
+
+#word = "hello world"
+
+#letters = 'c'
+
+
+#print(number)
+#print(decimalNumber)
+#print(word)
+#print(letters)
+
+#Integers
+
+numX = 20
+
+numY = 30
+
+numZ = numX + numY
+numA = numX * numY
+numB = numY / numX
+
+    
+
+print(numX)
+print(numY)
+print(numZ)
+print(numA)
+print(numB)
+
+#Strings
+
+#Characters or char
+
+#Floats or doubles(java)
+
+#if and elif and else statements
+    #AND or not
+
+
+#Lists and arrays
+
+#inputs and outputs
+
+#Functions   
